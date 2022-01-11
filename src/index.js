2fd445670673cb6d15aa8f35b8293c7be0308d58
Boris <bugaenko.boris@gmail.com>
2022-01-11 17:32:26 +0200

    feat: add function solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
-}
+    let a = Math.abs(n);
+    let res = '';
+    for(let i = String(a).length - 1; i>=0; i--){
+        res += String(a)[i];
+    }return Number(res);
+};
